fbbd58e95ca5a8e0a3cc8954f930e9369d109e83
Decklanorie <65791024+Decklanorie@users.noreply.github.com>
2023-04-29 23:27:30 +0100

    added firebase and toast

--- src/From/Form.js
+++ src/From/Form.js
@@ -4,6 +4,12 @@ import { FcGoogle } from "react-icons/fc";
 import "./Form.css";
 import Float from "../Sign/FloatAnime/Float";
 import React from "react";
+//added by decklan.
+import {signUp} from "../server/functions";
+//Run, npm install react-toastify
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function Form() {
   const [value, setValue] = useState(0);
@@ -13,15 +19,30 @@ function Form() {
     const formData = new FormData(e.currentTarget);
     // get values one by one
     const name = formData.get("name");
-    console.log(name);
-    // get all of them
-    const newUser = Object.fromEntries(formData);
-    // do something (post request, add to list, etc)
-    console.log(newUser);
-    // Gotcha - re-render won't clear out the values
+    const email = formData.get("email");
+    const password = formData.get("password");
+    
+//     check if any field is empty
+    if(name.length != '' && email.length != '' & password.length!= ''){
+      signUp(name,email,password).then(runState=>{
+        if(runState)
+        {toast.success('Sign up successful, procees to sign in page'); }
+        else{toast.error('Something went wrong, we couldn\'t sign u in.');}
+        e.currentTarget.reset();
+      });
+    }else{
+     toast.error('Please fill all fields'); 
+    }
+    
+//     console.log(name);
+//     // get all of them
+//     const newUser = Object.fromEntries(formData);
+//     // do something (post request, add to list, etc)
+//     console.log(newUser);
+//     // Gotcha - re-render won't clear out the values
     setValue(value + 1);
     // reset values
-    e.currentTarget.reset();
+    
   };
 
   return (
@@ -91,6 +112,8 @@ function Form() {
         <div className="imgi">
           <Float />
         </div>
+// this component is neccessary to display toast
+  <ToastContainer />
       </main>
     </>
   );
