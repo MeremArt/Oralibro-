bca8dcb30a291d160960647f535861b6714e0238
Merem-Lab <ugofranklin22@gmail.com>
2023-06-05 02:46:40 +0100

    improvement on discover page

--- src/Homepage/Global/Topbar.js
+++ src/Homepage/Global/Topbar.js
@@ -1,22 +1,30 @@
 import React from "react";
 import styled from "styled-components";
-import SearchBar from "./SearchBar";
+import { useLocation } from "react-router-dom";
+// import SearchBar from "./SearchBar";
+import Search from "./Search";
+
 export const Topbar = () => {
+  const location = useLocation();
+  const isDashboardRoute = location.pathname === "/oralibro/dashboard";
+
   return (
     <TopNav>
       <div>
         <section className="Topnav">
-          <h2>Oralibro</h2>
-          <SearchBar />
+          <div>
+            <h2>Oralibro</h2>
+          </div>
+          {isDashboardRoute && <Search />}
         </section>
       </div>
     </TopNav>
   );
 };
+
 const TopNav = styled.section`
   .Topnav {
     box-sizing: border-box;
-
     position: absolute;
     width: 100%;
     height: 55px;
@@ -25,8 +33,9 @@ const TopNav = styled.section`
     top: 0px;
     flex-direction: row;
     /* white */
-
     background: #fcfbfb;
     border-bottom: 0.5px solid #dddbdb;
   }
 `;
+
+export default Topbar;
